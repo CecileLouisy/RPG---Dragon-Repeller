1f39ce7c9e555b97453219a43a180ac731f1c9f7
CecileLouisy <cecile.louisy18@gmail.com>
2024-01-23 13:34:41 +0100

    last step

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const locations=[
     {
         name: "kill monster",
         "button text": ["Go to town square","Go to town square","Go to town square"],
-        "button functions": [goTown,goTown,goTown],
+        "button functions": [goTown,goTown,easterEgg],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
@@ -72,6 +72,12 @@ const locations=[
         "button text": ["REPLAY?","REPLAY?","REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
+    },
+    {
+        name: "easter egg",
+        "button text": ["2","8","Go to town square?"],
+        "button functions": [pickTwo,pickEight,goTown],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ];
 
@@ -186,6 +192,11 @@ function attack() {
     } else if (monsterHealth<=0) {
         fighting===2?winGame():defeatMonster();
     }
+    //Chance of breaking the weapon on every attack=0.1 & we don't want a player's only weapon to break
+    if (Math.random()<=.1&&inventory.length!==1){
+        text.innerText+=" Your "+inventory.pop()+" breaks." //remove (and return in the text) the last item in the inventory
+        currentWeapon--
+    }
 }
 
 function getMonsterAttackValue(level) {
@@ -195,6 +206,7 @@ function getMonsterAttackValue(level) {
 }
 
 function isMonsterHit() {
+    return Math.random()>.2||health<20;//The player hit if random 0-1>0.2 or health<20
 
 }
 
@@ -228,4 +240,40 @@ function restart() {
     healthText.innerText=health;
     xpText.innerText=xp;
     goTown();
+}
+
+function easterEgg() {
+    update(locations[7]);
+}
+
+function pick(guess) {
+    const numbers=[];
+    while(numbers.length<10) {
+    numbers.push(Math.floor(Math.random()*11));//push a random number between 0 and 10 in the end of the numbers array.    
+    }
+    text.innerText="You picked "+guess+". Here are the random numbers:\n";//\n new line for the next text.innerText
+    for(let i=0;i<10;i++) {
+        text.innerText+=numbers[i]+"\n";//
+    }
+    if(numbers.includes(guess)) {
+        console.log("The number is in the array.");
+        text.innerText+="Right! You win 20 gold!";
+        gold+=20;
+        goldText.innerText=gold;
+    } else {
+        text.innerText+="Wrong! You lose 10 health!";
+        health-=10;
+        healthText.innerText=health;
+        if (health<=0) {
+            lose();
+        }
+    }
+}
+
+function pickTwo() {
+    pick(2);
+}
+
+function pickEight() {
+    pick(8);
 }
